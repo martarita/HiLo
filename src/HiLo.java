f2e3ace3336245490701330383244ed79f3e483f
Marta <martasidor86@gmail.com>
2018-07-11 17:00:06 +0200

    removing errors

--- src/HiLo.java
+++ src/HiLo.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+
 public class HiLo {
 
 	public static void main(String[] args) {
@@ -6,24 +7,24 @@ public class HiLo {
 		Scanner scan = new Scanner(System.in);
 		String playAgain ="";
 		do {
-		// tworzenie liczby losowej do odgadnięcia
-		int theNumber = (int)(Math.random() *100+1);
-				System.out.println(theNumber);
-		int guess =0;
-		while (guess != theNumber){			
-			System.out.println("Guess a number beetween 1 and 100");
-			guess = scan.nextInt();
-			if (guess < theNumber)
-				System.out.println(guess +" is too low. Try again");
-			else if (guess > theNumber)
-				System.out.println(guess + " is too high. Try again");
-			else 
-				System.out.println(guess + " is correct. You win!");
-		}
-		System.out.println("Would you like to play again (y/n)");
-		playAgain= scan.next();
-		while (play Again.equalsIgnoreCase("y"));
-		}
+			// tworzenie liczby losowej do odgadnięcia
+			int theNumber = (int)(Math.random() *100+1);
+			System.out.println(theNumber);
+			int guess =0;
+			while (guess != theNumber){			
+				System.out.println("Guess a number beetween 1 and 100");
+				guess = scan.nextInt();
+				if (guess < theNumber)
+					System.out.println(guess +" is too low. Try again");
+				else if (guess > theNumber)
+					System.out.println(guess + " is too high. Try again");
+				else 
+					System.out.println(guess + " is correct. You win!");
+			}
+			System.out.println("Would you like to play again (y/n)");
+			playAgain= scan.next();
+		}while (playAgain.equalsIgnoreCase("y"));
+
 		}
 
-}
+	}
